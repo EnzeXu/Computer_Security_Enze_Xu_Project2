7d5410253f4c6668c5afaa4f35dde06e235ce25f
EnzeXu <xue20@wfu.edu>
2021-10-26 06:18:08 -0400

    test2

--- test2.c
+++ test2.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
-int main() {
-	char *env = getenv("RTL_SHELL");
-	if (env) {
-		printf("/bin/sh is at %x\n", (unsigned int) env);
-	}
+int main(int argc, char const *argv[]) {
+	char *p;
+	p = getenv(argv[1]);
+	p += (strlen(argv[0]) - strlen(argv[2])) * 2;
+	printf("%s at %p\n", argv[1], p);
 	return 0;
 }
