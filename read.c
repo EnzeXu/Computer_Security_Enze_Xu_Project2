d87454cb275a3e71aebab014205cd41029064d8b
EnzeXu <xue20@wfu.edu>
2021-10-25 22:01:10 -0400

    updates

--- read.c
+++ read.c
@@ -4,55 +4,30 @@
 #include <unistd.h>
 #include <ctype.h>
 
-void read1(void) {
-	FILE *fp = NULL;
-	fp = fopen("badfile", "r");
-	if (fp == NULL) {
-		perror("Error in opening file .myhistory");
-		return;
-	}
-	int count = 0;
-	int c;
-	int z = 517;
-	while(z--) {
-		fscanf(fp, "%x", &c);
-		printf("%4d ", c);
-		count ++;
-		if (count % 16 == 0) {
-			printf("\n");
-		}
-	}
-	printf("\n");
-	fclose(fp);
-	return;
-}
-
-void read2(void) {
-	FILE *sf;
-	sf = fopen("badfile", "rb");
+void myRead(void) {
+	FILE *fp;
+	fp = fopen("badfile", "rb");
 	int n;
 	unsigned char buf[16];
 	unsigned long count = 0;
-	while((n = fread(buf, sizeof(char), 16, sf)) > 0) {
-		int i;
+	while((n = fread(buf, sizeof(char), 16, fp)) > 0) {
 		printf("%08lx ", count);
-		for(i=0; i<n; i++) {
+		for(int i = 0; i < n; ++i) {
 			printf("%02x ", (unsigned)buf[i]);
 		}
-		if(n < 16) {
-			for(i=n; i<16; i++) printf("    ");
+		if (n < 16) {
+			for(int i = n; i < 16; ++i) printf("    ");
 		}
-		for(i=0; i<n; i++) {
+		for(int i = 0; i < n; ++i) {
 			putchar(isprint(buf[i]) ? buf[i] : '.');
 		}
 		putchar('\n');
 		count += 16;
 	}
-	fclose(sf);
+	fclose(fp);
 }
 
 int main() {
-	read2();
-	//read1();
+	myRead();
 	return 0;
 }
