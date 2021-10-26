537d06bdb010aedd89897b35bd17045884f1e01d
EnzeXu <xue20@wfu.edu>
2021-10-25 21:15:27 -0400

    updates

---
+++ read.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+void readHistory(void) {
+	FILE *fp = NULL;
+	fp = fopen("badfile", "r");
+	if (fp == NULL) {
+		perror("Error in opening file .myhistory");
+		return;
+	}
+	char c;
+	int count = 0;
+	while(c = fgetc(fp)) {
+		printf(%c);
+		count ++;
+		if (count % 20 == 0) {
+			printf("\n");
+		}
+	}
+	
+	fclose(fp);
+	return;
+}
+
+int main() {
+	return 0;
+}
